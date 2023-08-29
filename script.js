cd8f8210c5897c132587fc7fd07bdee12f6fd096
nguyen <tovannguyen12354@gmail.com>
2023-08-29 20:20:25 +0700

    bt6

---
+++ script.js
@@ -0,0 +1,8 @@
+function decimalToBinary(num) {
+    const binaryNumber = num.toString(2);
+    return binaryNumber;
+}
+
+const decimal = 10;
+const binary = decimalToBinary(decimal);
+console.log(binary);
